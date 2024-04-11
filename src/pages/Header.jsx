09e311118bff847e0b820f39591cbd98672a0993
SamuelLJG <samuelljg@outlook.com>
2024-04-11 00:10:01 -0300

    first commit

---
+++ src/pages/Header.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom"
+
+export default function Header(){
+    return<>
+    <Link to='react-default/'>Home</Link>
+    <Link to='react-default/about'>About</Link>
+    </>
+}

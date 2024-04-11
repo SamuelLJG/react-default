4e8b5dafe1e9e22477f6da07c6f704b9e7e97c7a
SamuelLJG <samuelljg@outlook.com>
2024-04-11 04:18:22 -0300

    netlify.toml

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home'
 import About from './pages/About';
@@ -9,13 +9,13 @@ import Header from './pages/Header';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Header/>
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
